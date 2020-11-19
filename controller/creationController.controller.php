f19fbfa3d81575d1ef743fca7123c29d3f865008
Ridmi97 <67148053+Ridmi97@users.noreply.github.com>
2020-11-19 22:26:32 +0530

    Use template design pattern to creation scenario

--- controller/creationController.controller.php
+++ controller/creationController.controller.php
@@ -1,128 +1,44 @@
 <?php
 include('../model/creationModel.model.php');
-//include('../model/adminModel.model.php');
-//include('../controller/adminController.controller.php');
 
+abstract class ICreation{
 
-/*$ob = new Admin();
-$re = $ob->getAdminInfo('1');
-
-$name= $re[0]['Name'];
-
-Interface IAdmin{
-
-	public function update(IUser $user_in);
-}
-
-
-Interface IUser{
-	public function attach(IAdmin $admin_in);
-	public function detach(IAdmin $admin_in);
-	public function notify();
-
-}
-
-
-class AdminObserver implements IAdmin{
-
-	private $staffID;
-	private $name;
-
-	public function AdminObserver($staffID,$name){
-		$this->staffID=$staffID;
-		$this->name=$name;
-
-	}
-
-	public function update(IUser $user_in){
-		$update=new Creation();
-		$updOb=$update->getCreationalInfo();
-		echo $updOb->rowCount();
-		
-
-	}
-}
-
-class UserSubject implements IUser{
-	private $adminObservers = array();
-
-	
-	public function attach(IAdmin $admin_in){
-		$this->adminObservers[] = $admin_in;
-
-	}
-
-	public function detach(IAdmin $admin_in){
-		foreach ($this->adminObservers as $okey => $oval) {
-			if($oval==$admin_in){
-				unset($this->adminObservers[$okey]);
-
-			}
-		}
-	}
-
-
-	public function notify(){
-		foreach ($this->adminObservers as $obs) {
-			$obs->update($this);
-		}
-	}
-}
-
-$adObs=new AdminObserver('1',$name);
-$userSub = new UserSubject();
-$userSub->attach($adObs);
-$userSub->notify();
-
-
-
-?>
-*/
-Interface ICreation{
-	public function setInfo($me,$fileName,$fileTmpName);
-	public function addCreation();
-}
-
-class AdminCreation implements ICreation{
-	private $me;
-	private $fileName;
-	private $fileActualExt;
+	public $me;
+	public $fileName;
+	public $fileActualExt;
 
 	public function setInfo($me,$fileName,$fileActualExt){
-		
 		$this->me=$me;
 		$this->fileName=$fileName;
 		$this->fileActualExt=$fileActualExt;
+
 	}
+	public abstract function addCreation();
+}
 
+class AdminCreation extends ICreation{
 
-	public function addCreation()
-    {
+	public function setInfo($me,$fileName,$fileActualExt){
+		parent::setInfo($me,$fileName,$fileActualExt);
+	}
+	
+	public function addCreation(){
+    
     	$o=new Creation();
     	$str='1';
-    	$o->addCreationalInfo($this->me,$this->fileName,$this->fileActualExt,$str);
+		$o->addCreationalInfo($this->me,$this->fileName,$this->fileActualExt,$str);
     }
 }
 
 
-class UserCreation implements ICreation{
-
-	private $me;
-	private $fileName;
-	private $fileActualExt;
+class UserCreation extends ICreation{
 
 	public function setInfo($me,$fileName,$fileActualExt){
-		
-		$this->me=$me;
-		$this->fileName=$fileName;
-		$this->fileActualExt=$fileActualExt;
-
-		
+		parent::setInfo($me,$fileName,$fileActualExt);
 	}
 
-	public function addCreation()
-    {
-    	
+	public function addCreation(){
+    
     	$o=new Creation();
     	$str='0';
     	$o->addCreationalInfo($this->me,$this->fileName,$this->fileActualExt,$str);
@@ -135,7 +51,6 @@ class CreationMaker{
 	private $common;
 	
 
-
 	public function CreationMaker(ICreation $common){
 		$this->common = $common;
 		
@@ -145,11 +60,8 @@ class CreationMaker{
 
 	public function setInfo($me,$fileName,$fileActualExt){
 		
-		
 		$this->common->setInfo($me,$fileName,$fileActualExt);
 		
-
-		return $this;
 	}
 
 	public function addCreation(){
@@ -163,15 +75,3 @@ class CreationMaker{
 
 }
 
-class Notification{
-
-	public function notify(){
-		$update=new Creation();
-		$updOb=$update->getCreationalInfo();
-		echo $updOb->rowCount();
-
-	}
-}
-
-$obj=new Notification();
-$obj->notify();
